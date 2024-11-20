c4e512b3baf2f3227d050da43e56569bef6c9ff2
asistradition <cj59@nyu.edu>
2024-11-20 11:43:37 -0500

    Add TruncMinMaxScaler

--- scself/scaling/truncscaler.py
+++ scself/scaling/truncscaler.py
@@ -3,7 +3,8 @@ import scipy.sparse as sps
 
 from sklearn.preprocessing import (
     RobustScaler,
-    StandardScaler
+    StandardScaler,
+    MinMaxScaler
 )
 
 
@@ -11,7 +12,7 @@ class TruncRobustScaler(RobustScaler):
 
     def fit(self, X, y=None):
 
-        if isinstance(X, (sps.csr_matrix, sps.csc_array)):
+        if isinstance(X, (sps.csr_matrix, sps.csr_array)):
             # Use custom extractor to turn X into a CSC with no
             # indices array; RobustScaler makes an undesirabe
             # CSR->CSC conversion
@@ -53,3 +54,30 @@ class TruncStandardScaler(StandardScaler):
         self.scale_[self.var_ <= 1] = 1
 
         return self
+
+
+class TruncMinMaxScaler(MinMaxScaler):
+
+    def __init__(self, feature_range=(0, 1), quantile_range=0.99, *, copy=True, clip=False):
+        self.feature_range = feature_range
+        self.quantile_range = quantile_range
+        self.copy = copy
+        self.clip = True
+
+    def fit(self, X, y=None):
+
+        super().fit(X, y)
+
+        self.data_min_ = 0
+        self.data_max_ = np.nanquantile(
+            X, self.quantile_range, axis=0
+        )
+        self.data_range_ = self.data_max_ - self.data_min_
+
+        _fixed_range = self.data_range_.copy()
+        _fixed_range[_fixed_range == 0] = 1.
+
+        self.scale_ = self.feature_range[1] - self.feature_range[0]
+        self.scale_ /= _fixed_range
+
+        self.min_ = self.feature_range[0]
